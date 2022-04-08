3f6b5ea274b45eed45d57eebdca4f4493efa0f5b
Ilya Litvin <ltvin@yandex.ru>
2022-04-08 09:08:40 +0300

    database 2.2 - 1 done

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,7 +6,10 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    list_student = Student.objects.all().prefetch_related('teacher')
+    context = {
+        'object_list': list_student
+    }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
